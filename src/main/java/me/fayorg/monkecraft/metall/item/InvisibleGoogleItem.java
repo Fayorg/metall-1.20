878bc5abb5ea26c8dc0f451d25c65ac22eb13ba0
Fayorg <ebr@aketechnology.com>
2024-01-07 01:56:50 +0100

    add: use to equip

--- src/main/java/me/fayorg/monkecraft/metall/item/InvisibleGoogleItem.java
+++ src/main/java/me/fayorg/monkecraft/metall/item/InvisibleGoogleItem.java
@@ -1,16 +1,25 @@
 package me.fayorg.monkecraft.metall.item;
 
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
-
-import java.awt.*;
 
 public class InvisibleGoogleItem extends Item implements ICurioItem {
     public InvisibleGoogleItem() {
         super(new Item.Properties().stacksTo(1).rarity(Rarity.RARE));
     }
 
+    @Override
+    public boolean canEquipFromUse(SlotContext slotContext, ItemStack stack) {
+        return true;
+    }
 
+    @Override
+    public void onEquipFromUse(SlotContext slotContext, ItemStack stack) {
+        slotContext.entity().playSound(SoundEvents.ARMOR_EQUIP_IRON, 1.0F, 1.0F);
+    }
 
 }
