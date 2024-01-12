6898539557db24c45e22f9c31e3db984f76644a0
Fayorg <ebr@aketechnology.com>
2024-01-12 22:35:44 +0100

    fix: reordering metall CreativeModTab

--- src/main/java/me/fayorg/monkecraft/metall/item/MetallCreativeModTabs.java
+++ src/main/java/me/fayorg/monkecraft/metall/item/MetallCreativeModTabs.java
@@ -1,9 +1,12 @@
 package me.fayorg.monkecraft.metall.item;
 
+import me.fayorg.monkecraft.metall.api.EnergyUtil;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -12,14 +15,30 @@ public class MetallCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, "metall");
 
     public static final RegistryObject<CreativeModeTab> METALL_TAB = CREATIVE_MODE_TABS.register("metall", () -> CreativeModeTab.builder().icon(() -> new ItemStack(MetallItems.SLINGSHOT.get())).title(Component.translatable("itemGroup.metall")).displayItems((pParameters, pOutput) -> {
+        // Tools
         pOutput.accept(MetallItems.SLINGSHOT.get());
-        pOutput.accept(MetallItems.CHOCOLATE.get());
-        pOutput.accept(MetallItems.PIEROGI.get());
-        pOutput.accept(MetallItems.SLIME_BOOTS.get());
-        pOutput.accept(MetallItems.INVISIBLE_GOGGLES.get());
+        pOutput.accept(setItemStackMaxCharge(MetallItems.INVISIBLE_GOGGLES.get()));
 
+        // Armors
+        pOutput.accept(MetallItems.SLIME_BOOTS.get());
+
+        // Foods
+        pOutput.accept(MetallItems.CHOCOLATE.get());
+        pOutput.accept(MetallItems.DOUGH.get());
+
+        ItemStack pierogi = new ItemStack(MetallItems.PIEROGI.get());
+        PierogiItem.setFoodProperties(pierogi, 6, 1.0F, false);
+        pOutput.accept(pierogi);
+
+        // Blocks
         pOutput.accept(MetallItems.FERTILIZER_BLOCK_ITEM.get());
     }).build());
+
+    private static ItemStack setItemStackMaxCharge(ItemLike item) {
+        ItemStack stack = new ItemStack(item);
+        EnergyUtil.setFull(stack);
+        return stack;
+    }
 
 
 }
